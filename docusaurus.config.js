09e46c8249e329432b7369c41a2f5be13830e046
Brian Jensen <bsj@enterspeed.com>
2022-03-22 13:46:21 +0100

    Added overview page and changed navigation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,7 +95,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
             },
             {
               type: "doc",
-              docId: "tutorials/umbraco-nextjs/intro",
+              docId: "tutorials/overview",
               label: "Tutorials",
               position: "left",
             },
