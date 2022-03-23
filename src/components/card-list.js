bc6844c5456da1b776b29c2ad2df21edd10273c3
Brian Jensen <bsj@enterspeed.com>
2022-03-23 14:13:49 +0100

    Aligned naming and styling for easier use and maintenance

--- src/components/card-list.js
+++ src/components/card-list.js
@@ -1,21 +1,22 @@
 import React from "react";
 
-const CardList = ({ title, content, link }) => {
+const CardListItem = ({ title, children, link }) => {
   return (
-    <div class="es-card-list">
-      <div class="es-card-list__heading">
-        <h4>{title}</h4>
-      </div>
-      <div class="es-card-list__content">
-        <p>{content}</p>
-        <p><a href={link}>{title}</a></p>
-      </div>
+    <div className="es-card-list-item">
+      <a href={link} title="{title}">
+        <div className="es-card-list-item__title">
+          <h4>{title}</h4>
+        </div>
+        <div className="es-card-list-item__content">
+          {children}
+        </div>
+      </a>
     </div>
   );
 };
 
 const CardListContainer = ({ children }) => {
-  return <div className="es-card-list__container">{children}</div>;
+  return <div className="es-card-container">{children}</div>;
 };
 
-export { CardList, CardListContainer };
+export { CardListItem, CardListContainer };
